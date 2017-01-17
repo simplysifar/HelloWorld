981dd763236712209037d7429e7c1b580e7f3257
simplysifar <simplysifar@gmail.com>
2017-01-18 00:11:04 +1100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by varun on 1/17/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        int i = 5;
+        System.out.println("Hello World");
+        System.out.println("Hello Tim"+ "\n"+i);
+        }
+}

4360509f8c02af8e4959feda31e872fef566ad04
simplysifar <simplysifar@gmail.com>
2017-01-19 23:26:49 +1100

    hi

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,21 @@
 /**
  * Created by varun on 1/17/2017.
  */
+
+
 public class HelloWorld {
 
-    public static void main(String[] args){
-        int i = 5;
+      public static void main(String[] args){
+        int i =5;
+        String A="Hello Me";
+
         System.out.println("Hello World");
-        System.out.println("Hello Tim"+ "\n"+i);
+        System.out.println(args[0]);
+
+       }
+
+
+
         }
-}
+
+
